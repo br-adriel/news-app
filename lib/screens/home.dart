f13db70da8086c2ca19f8a39c6af2f1846a80e33
Adriel <adriel.fsantos@outlook.com>
2023-06-16 22:17:13 -0300

    Adiciona modificador const a chamada de NewsScreen #4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,7 +24,7 @@ class HomeScreen extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => NewsScreen(),
+        builder: (context) => const NewsScreen(),
       ),
     );
   }
