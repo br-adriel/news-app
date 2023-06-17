b4361c31d3bd919f76df553964dd02a6fd5a02e9
Adriel <adriel.fsantos@outlook.com>
2023-06-17 19:35:00 -0300

    Implementa layout da tela News #7

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/widgets/barra_superior.dart';
+import 'package:news_app/widgets/botoes/botao_ler_artigo.dart';
+import 'package:news_app/widgets/cover_image.dart';
+import 'package:news_app/widgets/news_screen_layout/news_first_paragraph.dart';
+import 'package:news_app/widgets/news_screen_layout/news_info.dart';
+import 'package:news_app/widgets/news_screen_layout/news_title_abstract.dart';
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
@@ -7,10 +11,43 @@ class NewsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BarraSuperior(
-        titulo: 'Noticia #1',
+      extendBodyBehindAppBar: true,
+      floatingActionButton:
+          const BotaoLerArtigo(urlArtigo: 'https://google.com'),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            flexibleSpace: const FlexibleSpaceBar(
+              background: CoverImage(imageURL: ''),
+            ),
+            expandedHeight: MediaQuery.of(context).size.height * 0.3,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 32,
+                    left: 16,
+                    right: 16,
+                    bottom: 80,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      NewsTitleAbstract(),
+                      SizedBox(height: 16),
+                      NewsInfo(),
+                      SizedBox(height: 20),
+                      NewsFirstParagraph(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: const Text("Noticia"),
     );
   }
 }
