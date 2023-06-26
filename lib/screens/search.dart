d2967b70c4cb74e23fcbf1bdbe4d7cd7bffa4280
Adriel <adriel.fsantos@outlook.com>
2023-06-25 23:48:29 -0300

    Altera conteudo de tela search #9

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/widgets/barra_superior.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BarraSuperior(titulo: "Search articles"),
-    );
+    return Observer(builder: (_) {
+      return const Scaffold(
+        body: Text("Resultados da busca"),
+      );
+    });
   }
 }
