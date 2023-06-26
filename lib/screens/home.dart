65c4630f7f2a259fc38e4e96247dba3eb304ea7d
Adriel <adriel.fsantos@outlook.com>
2023-06-25 23:48:53 -0300

    Adiciona protótipo de barra de busca #9

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:news_app/controller/home_screen.dart';
+import 'package:news_app/controller/searchbar.dart';
 import 'package:news_app/models/noticia_model.dart';
 import 'package:news_app/widgets/barra_superior.dart';
 import 'package:news_app/widgets/bottom_navigation_bar_widget.dart';
@@ -9,6 +10,8 @@ import 'package:news_app/widgets/menus_popup/menu_periodo.dart';
 
 class HomeScreen extends StatelessWidget {
   final HomeScreenController homeScreenController = HomeScreenController();
+  final SearchbarController searchbarController = SearchbarController();
+
   final Noticia noticiaTeste = Noticia.fromJson(
     {
       "abstract":
@@ -94,11 +97,11 @@ class HomeScreen extends StatelessWidget {
     return Observer(builder: (_) {
       return Scaffold(
         appBar: BarraSuperior(
-          botaoPesquisar: true,
           titulo: homeScreenController.titulo,
           menuPopup: homeScreenController.secaoAtiva == SecaoAtiva.populares
               ? MenuPeriodo()
               : null,
+          searchbarController: searchbarController,
         ),
         body: Padding(
             padding: const EdgeInsets.all(8),
